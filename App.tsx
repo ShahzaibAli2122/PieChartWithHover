e583d9895b426d665531d80fad4f3cc8df5aed17
shahzaibali2122 <shahzaibkarim74@gmail.com>
2024-02-21 18:43:21 +0500

    Final Chart

--- App.tsx
+++ App.tsx
@@ -3,13 +3,9 @@ import {NavigationContainer} from '@react-navigation/native';
 // import TabNavigator from './src/Navigation/TabNavigator';
 
 import Charts from './src/SchoolInnerPieChart'
-import { Text, View } from 'react-native';
 function App(): JSX.Element {
   return (
-    <View>
-      <Charts />
-      <Text>Hello</Text>
-    </View>
+   <Charts />
   );
 }
 
