4e77d67ce845bcd16b915dad151ab1efeecd8986
shahzaibali2122 <shahzaibkarim74@gmail.com>
2024-02-24 18:32:41 +0500

    Charts

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+// import TabNavigator from './src/Navigation/TabNavigator';
+
+import Charts from './src/SchoolInnerPieChart'
+function App(): JSX.Element {
+  return (
+   <Charts />
+  );
+}
+
+export default App;
